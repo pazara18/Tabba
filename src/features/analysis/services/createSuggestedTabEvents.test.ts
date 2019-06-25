ed55c961f1271d3ccb4cc4e36083869ea1feff6e
pazara18 <43823072+pazara18@users.noreply.github.com>
2019-06-25 14:40:00 +0900

    chore: guitar

--- src/features/analysis/services/createSuggestedTabEvents.test.ts
+++ src/features/analysis/services/createSuggestedTabEvents.test.ts
@@ -10,3 +10,19 @@ describe("createSuggestedTabEvents", () => {
           confidence: 0.82,
           durationSeconds: 0.5,
           frequencyHz: 164.81,
+          pitch: "E3",
+          startSeconds: 1.25,
+        },
+      ],
+      standardGuitarTuning,
+      { createId: () => "suggested-1" }
+    );
+
+    expect(events).toHaveLength(1);
+    expect(events[0]).toMatchObject({
+      id: "suggested-1",
+      startSeconds: 1.25,
+      durationSeconds: 0.5,
+      kind: "single",
+      texture: "mono",
+      detectedPitches: [{ pitch: "E3", confidence: 0.82, frequencyHz: 164.81 }],
