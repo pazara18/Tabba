b845f03cbde5d10b5a66ac5342af9647e7f77947
pazara18 <43823072+pazara18@users.noreply.github.com>
2019-08-22 10:52:00 +0900

    fix: onset-detection

--- src/features/analysis/services/instrumentAnalysisOptions.test.ts
+++ src/features/analysis/services/instrumentAnalysisOptions.test.ts
@@ -2,3 +2,6 @@ import { describe, expect, it } from "vitest";
 import { getInstrumentPitchOptions } from "./instrumentAnalysisOptions";
 
 describe("instrumentAnalysisOptions", () => {
+  it("uses a lower and steadier pitch range for bass", () => {
+    expect(getInstrumentPitchOptions("bass")).toMatchObject({
+      frameSize: 4096,
