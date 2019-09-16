6101c1ec5c494bb2d30e286f2852f56740ab6675
pazara18 <43823072+pazara18@users.noreply.github.com>
2019-09-16 14:03:00 +0900

    chore: onset-detection

--- src/features/analysis/services/instrumentAnalysisOptions.ts
+++ src/features/analysis/services/instrumentAnalysisOptions.ts
@@ -1,2 +1,4 @@
 import type { InstrumentKind } from "../../../domain/instruments/types";
 import type { PitchDetectionOptions } from "./pitchDetection";
+
+export function getInstrumentPitchOptions(instrument: InstrumentKind): PitchDetectionOptions {
