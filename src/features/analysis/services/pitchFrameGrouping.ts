c4938ff8de591e3717788706fd2eb970a59ec803
pazara18 <43823072+pazara18@users.noreply.github.com>
2020-01-08 16:24:00 +0900

    style: clean up onset-detection

--- src/features/analysis/services/pitchFrameGrouping.ts
+++ src/features/analysis/services/pitchFrameGrouping.ts
@@ -11,3 +11,19 @@ const defaultGroupingOptions = {
   minDurationSeconds: 0.08,
   pitchWobbleMergeSeconds: 0.18,
   pitchWobbleSemitones: 1,
+};
+
+export function groupPitchFrames(
+  frames: PitchFrame[],
+  options: PitchDetectionOptions = {}
+): DetectedNote[] {
+  const settings = { ...defaultGroupingOptions, ...options };
+  const groups = mergePitchWobbleGroups(createPitchFrameGroups(frames, settings), settings);
+
+  return groups.flatMap((group) => createDetectedNote(group, settings.minDurationSeconds));
+}
+
+function createPitchFrameGroups(
+  frames: PitchFrame[],
+  settings: Required<typeof defaultGroupingOptions>
+): PitchFrameGroup[] {
