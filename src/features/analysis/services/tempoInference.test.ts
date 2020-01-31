2fc6f99106d1a492a6b08aabeea1cb2aece7d3ef
pazara18 <43823072+pazara18@users.noreply.github.com>
2020-01-31 09:27:00 +0900

    perf: improve guitar

--- src/features/analysis/services/tempoInference.test.ts
+++ src/features/analysis/services/tempoInference.test.ts
@@ -5,3 +5,11 @@ function generateBeatOnsets(bpm: number, count: number, offset = 0): number[] {
   const beatSeconds = 60 / bpm;
   return Array.from({ length: count }, (_, index) => offset + index * beatSeconds);
 }
+
+describe("estimateTempoFromOnsets", () => {
+  it("returns undefined when there are fewer than four onsets", () => {
+    expect(estimateTempoFromOnsets([])).toBeUndefined();
+    expect(estimateTempoFromOnsets([0, 0.5, 1])).toBeUndefined();
+  });
+
+  it("recovers a 120 bpm grid from clean onsets", () => {
