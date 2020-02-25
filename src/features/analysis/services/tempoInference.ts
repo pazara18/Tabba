b71250f8b8ae6231c2ef4aad4010ae0bb8ef77db
rShimizu1988 <309014079+rShimizu1988@users.noreply.github.com>
2020-02-25 16:27:00 +0900

    fix: guitar

--- src/features/analysis/services/tempoInference.ts
+++ src/features/analysis/services/tempoInference.ts
@@ -15,3 +15,23 @@ const DEFAULT_MAX_BPM = 200;
 const DEFAULT_TOLERANCE_RATIO = 0.18;
 const BPM_QUANTIZATION = 0.5;
 const MIN_ONSETS_REQUIRED = 4;
+const MAX_PHASE_REFERENCES = 8;
+const HARMONIC_FACTORS = [0.5, 2 / 3, 1, 1.5, 2];
+
+export function estimateTempoFromOnsets(
+  onsetsSeconds: number[],
+  options: TempoInferenceOptions = {}
+): TempoEstimate | undefined {
+  const minBpm = options.minBpm ?? DEFAULT_MIN_BPM;
+  const maxBpm = options.maxBpm ?? DEFAULT_MAX_BPM;
+  const toleranceRatio = options.toleranceRatio ?? DEFAULT_TOLERANCE_RATIO;
+
+  if (onsetsSeconds.length < MIN_ONSETS_REQUIRED) {
+    return undefined;
+  }
+
+  const sorted = [...onsetsSeconds].sort((left, right) => left - right);
+  const candidates = collectBpmCandidates(sorted, minBpm, maxBpm);
+
+  if (candidates.size === 0) {
+    return undefined;
