1b693c2c35d929d9ef887d6033879d8f5afe560b
pazara18 <43823072+pazara18@users.noreply.github.com>
2020-05-05 09:34:00 +0900

    docs: document audio-analysis

--- src/features/audio/hooks/useDecodedWaveform.ts
+++ src/features/audio/hooks/useDecodedWaveform.ts
@@ -3,3 +3,10 @@ import { decodeAudioFile } from "../browser/decodeAudioFile";
 import { createWaveformPeaks, type WaveformPeak } from "../services/waveformPeaks";
 
 interface DecodedWaveformState {
+  error?: string;
+  isLoading: boolean;
+  peaks: WaveformPeak[];
+}
+
+const peakCount = 96;
+
