553e6b3b3bac36e560c213d729662587c82dedf8
pazara18 <43823072+pazara18@users.noreply.github.com>
2020-06-19 09:12:00 +0900

    perf: improve typescript

---
+++ src/features/audio/hooks/useProjectTransport.ts
@@ -0,0 +1,47 @@
+import { useCallback, useEffect, useRef, useState } from "react";
+import type { LoopRegion } from "../../editor/services/practiceControls";
+import {
+  normalizeLoopRegion,
+  normalizePlaybackRate,
+} from "../../editor/services/practiceControls";
+import {
+  computeStemGain,
+  getStemMix,
+  isAnyStemSoloed,
+  type StemMix,
+} from "../services/stemMixState";
+import type { RuntimeStemSource } from "../types";
+
+interface UseProjectTransportOptions {
+  sources: RuntimeStemSource[];
+  mixStates: Record<string, StemMix>;
+  loopRegion?: LoopRegion;
+  onStemDurationChange?: (stemId: string, durationSeconds: number) => void;
+  playbackRate?: number;
+}
+
+interface BufferEntry {
+  buffer: AudioBuffer;
+  fileToken: File;
+}
+
+interface SourceEntry {
+  source: AudioBufferSourceNode;
+  gain: GainNode;
+}
+
+interface PlaybackAnchor {
+  bufferStartOffset: number;
+  contextStartTime: number;
+  playbackRate: number;
+}
+
+const SOURCE_START_LEAD_SECONDS = 0.04;
+
+export function useProjectTransport({
+  sources,
+  mixStates,
+  loopRegion,
+  onStemDurationChange,
+  playbackRate = 1,
+}: UseProjectTransportOptions) {
