fe37f73f2ab267d956ff4bb9450d67ad22e1dc9d
pazara18 <43823072+pazara18@users.noreply.github.com>
2020-07-03 14:35:00 +0900

    test: cover guitar

--- src/features/audio/hooks/useRuntimeStemSources.ts
+++ src/features/audio/hooks/useRuntimeStemSources.ts
@@ -2,3 +2,10 @@ import { useCallback, useState } from "react";
 import type { Stem } from "../../project/types";
 import { createRuntimeStemImports } from "../services/createRuntimeStemImports";
 import type { RuntimeStemSource } from "../types";
+
+interface UseRuntimeStemSourcesOptions {
+  existingStems?: Stem[];
+  onStemsCreated: (stems: Stem[]) => void;
+}
+
+export function useRuntimeStemSources({
