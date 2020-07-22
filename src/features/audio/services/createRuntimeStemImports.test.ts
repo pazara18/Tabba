c527a32be12147f246d5c5d55ec34a5d7baf1fd1
pazara18 <43823072+pazara18@users.noreply.github.com>
2020-07-22 17:30:00 +0900

    test: cover webaudio

--- src/features/audio/services/createRuntimeStemImports.test.ts
+++ src/features/audio/services/createRuntimeStemImports.test.ts
@@ -3,3 +3,6 @@ import { createProjectFixture } from "../../../test/fixtures/createProjectFixtur
 import { createRuntimeStemImports } from "./createRuntimeStemImports";
 
 describe("createRuntimeStemImports", () => {
+  it("dedupes duplicate files imported in the same batch", () => {
+    const file = new File(["audio"], "lead.wav", { type: "audio/wav" });
+    const imports = createRuntimeStemImports([file, file], []);
