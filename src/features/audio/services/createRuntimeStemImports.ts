96df31d5fbef42aa548cb34cc5cf7778c61912d8
pazara18 <43823072+pazara18@users.noreply.github.com>
2020-08-24 15:27:00 +0900

    docs: document guitar

--- src/features/audio/services/createRuntimeStemImports.ts
+++ src/features/audio/services/createRuntimeStemImports.ts
@@ -1,2 +1,6 @@
 import type { Stem } from "../../project/types";
 import { createStemFromAudioFile } from "./createStemFromAudioFile";
+import { findMatchingStemForAudioFile } from "./matchStemSource";
+import type { RuntimeStemSource } from "../types";
+
+export interface RuntimeStemImport {
