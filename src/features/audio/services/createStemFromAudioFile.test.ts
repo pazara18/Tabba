7b1ef1a313e288183cb8d2449684c0d53c22513b
pazara18 <43823072+pazara18@users.noreply.github.com>
2020-08-31 13:29:00 +0900

    feat: guitar

--- src/features/audio/services/createStemFromAudioFile.test.ts
+++ src/features/audio/services/createStemFromAudioFile.test.ts
@@ -3,3 +3,10 @@ import { createStemFromAudioFile } from "./createStemFromAudioFile";
 
 describe("createStemFromAudioFile", () => {
   it("creates portable stem metadata from a local audio file", () => {
+    const stem = createStemFromAudioFile(
+      {
+        name: "lead-guitar.wav",
+        type: "audio/wav",
+        size: 4096,
+        lastModified: 1770000000000,
+      },
