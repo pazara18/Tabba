403d64a28af0ce3afef9761320726b8e6cad0589
pazara18 <43823072+pazara18@users.noreply.github.com>
2020-10-14 15:15:00 +0900

    fix: correct local-first

--- src/features/audio/services/createStemFromAudioFile.ts
+++ src/features/audio/services/createStemFromAudioFile.ts
@@ -1,3 +1,8 @@
 import type { Stem } from "../../project/types";
 import type { AudioStemFile } from "../types";
 
+interface CreateStemOptions {
+  createId?: () => string;
+}
+
+const defaultCreateId = () => crypto.randomUUID();
