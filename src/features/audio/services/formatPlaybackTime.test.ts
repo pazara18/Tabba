a219a31c5ea124e427fc1ff7943cbf1c477aca49
pazara18 <43823072+pazara18@users.noreply.github.com>
2020-11-07 16:29:00 +0900

    feat: add audio-analysis

--- src/features/audio/services/formatPlaybackTime.test.ts
+++ src/features/audio/services/formatPlaybackTime.test.ts
@@ -5,3 +5,5 @@ describe("formatPlaybackTime", () => {
   it("formats seconds as minutes, seconds, and milliseconds", () => {
     expect(formatPlaybackTime(0)).toBe("0:00.000");
     expect(formatPlaybackTime(1.25)).toBe("0:01.250");
+    expect(formatPlaybackTime(61.005)).toBe("1:01.005");
+  });
