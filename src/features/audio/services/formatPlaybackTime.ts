d33185bad0a7525d41d44db1e51cefafbb1a19e7
Hoffmann68 <309025925+Hoffmann68@users.noreply.github.com>
2020-11-22 11:37:00 +0900

    refactor: audio-analysis

--- src/features/audio/services/formatPlaybackTime.ts
+++ src/features/audio/services/formatPlaybackTime.ts
@@ -1,3 +1,5 @@
 export function formatPlaybackTime(totalSeconds: number): string {
   const safeSeconds = Math.max(0, totalSeconds);
   const minutes = Math.floor(safeSeconds / 60);
+  const seconds = Math.floor(safeSeconds % 60);
+  const milliseconds = Math.floor((safeSeconds % 1) * 1000);
