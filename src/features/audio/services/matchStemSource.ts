4a14089018a67abf51fd8d7f90519ca8dbef338c
pazara18 <43823072+pazara18@users.noreply.github.com>
2020-12-14 13:59:00 +0900

    chore: local-first

--- src/features/audio/services/matchStemSource.ts
+++ src/features/audio/services/matchStemSource.ts
@@ -2,3 +2,6 @@ import type { Stem } from "../../project/types";
 import type { AudioStemFile } from "../types";
 
 export function findMatchingStemForAudioFile(
+  file: AudioStemFile,
+  stems: Stem[]
+): Stem | undefined {
