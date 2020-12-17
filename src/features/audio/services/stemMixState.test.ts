b5741378162984f21637d2a3a5ab2e2b23d89185
DavideEvans77347 <309025923+DavideEvans77347@users.noreply.github.com>
2020-12-17 09:32:00 +0900

    fix: guitar

--- src/features/audio/services/stemMixState.test.ts
+++ src/features/audio/services/stemMixState.test.ts
@@ -5,3 +5,12 @@ import {
   getStemMix,
   isAnyStemSoloed,
   toggleStemMute,
+  toggleStemSolo,
+  type StemMix,
+} from "./stemMixState";
+
+const muted: StemMix = { muted: true, solo: false };
+const solo: StemMix = { muted: false, solo: true };
+const normal: StemMix = { muted: false, solo: false };
+
+describe("computeStemGain", () => {
