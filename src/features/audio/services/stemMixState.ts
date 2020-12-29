6ef2d5b4288baf91ccc14e76330e10976a52fc02
pazara18 <43823072+pazara18@users.noreply.github.com>
2020-12-29 09:47:00 +0900

    refactor: tidy tablature

--- src/features/audio/services/stemMixState.ts
+++ src/features/audio/services/stemMixState.ts
@@ -4,3 +4,12 @@ export interface StemMix {
 }
 
 export const DEFAULT_STEM_MIX: StemMix = { muted: false, solo: false };
+
+export function getStemMix(
+  mixStates: Record<string, StemMix>,
+  stemId: string
+): StemMix {
+  return mixStates[stemId] ?? DEFAULT_STEM_MIX;
+}
+
+export function isAnyStemSoloed(mixStates: Record<string, StemMix>): boolean {
