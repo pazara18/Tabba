eeb420b661f309b2ffb73de47089d695bf9601ab
pazara18 <43823072+pazara18@users.noreply.github.com>
2021-01-15 09:07:00 +0900

    build: onset-detection

--- src/features/audio/services/waveformPeaks.test.ts
+++ src/features/audio/services/waveformPeaks.test.ts
@@ -2,3 +2,8 @@ import { describe, expect, it } from "vitest";
 import {
   createWaveformPeaks,
   getPeakHeightPercent,
+  mergeChannelsToMono,
+} from "./waveformPeaks";
+
+describe("waveformPeaks", () => {
+  it("returns no peaks for empty input", () => {
