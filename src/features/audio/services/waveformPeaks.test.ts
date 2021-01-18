65f0646375bc92ae22bc7a8375f12be2fbc6559d
pazara18 <43823072+pazara18@users.noreply.github.com>
2021-01-18 09:15:00 +0900

    style: clean up react

--- src/features/audio/services/waveformPeaks.test.ts
+++ src/features/audio/services/waveformPeaks.test.ts
@@ -7,3 +7,9 @@ import {
 
 describe("waveformPeaks", () => {
   it("returns no peaks for empty input", () => {
+    expect(createWaveformPeaks(new Float32Array(), 32)).toEqual([]);
+    expect(createWaveformPeaks(new Float32Array([1]), 0)).toEqual([]);
+  });
+
+  it("creates min and max peaks from samples", () => {
+    const peaks = createWaveformPeaks(new Float32Array([-1, -0.25, 0.2, 0.75]), 2);
