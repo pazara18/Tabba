fd74f03657659e895726f9e0372eef462a60961e
pazara18 <43823072+pazara18@users.noreply.github.com>
2021-01-23 10:21:00 +0900

    refactor: tidy typescript

--- src/features/audio/services/waveformPeaks.ts
+++ src/features/audio/services/waveformPeaks.ts
@@ -3,3 +3,11 @@ export interface WaveformPeak {
   min: number;
 }
 
+export function createWaveformPeaks(samples: Float32Array, peakCount: number): WaveformPeak[] {
+  if (peakCount <= 0 || samples.length === 0) {
+    return [];
+  }
+
+  const samplesPerPeak = samples.length / peakCount;
+
+  return Array.from({ length: peakCount }, (_, index) => {
