0a165843f1df4b6037c34bbe22b46b7361443ac2
pazara18 <43823072+pazara18@users.noreply.github.com>
2021-03-15 16:07:00 +0900

    docs: document react

---
+++ src/features/editor/components/EditorWorkspace.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import type { InstrumentKind } from "../../../domain/instruments/types";
+import { StemLane } from "../../audio/components/StemLane";
+import { useProjectTransport } from "../../audio/hooks/useProjectTransport";
+import { useDecodedWaveform } from "../../audio/hooks/useDecodedWaveform";
+import type { StemMix } from "../../audio/services/stemMixState";
+import type { RuntimeStemSource } from "../../audio/types";
+import type { TabbaProject } from "../../project/types";
+import type { EventPopoverPatch } from "./EventPopover";
+import { TabStaffPanel } from "./TabStaffPanel";
+import { TimelinePanel } from "./TimelinePanel";
+import { TransportStrip } from "./TransportStrip";
+import styles from "./EditorWorkspace.module.css";
+import type { SelectedTabEvent } from "../types";
+import { createDefaultLoopRegion, normalizePlaybackRate } from "../services/practiceControls";
