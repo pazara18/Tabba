1872948537ee55ee7d46c82187898931e1efd057
pazara18 <43823072+pazara18@users.noreply.github.com>
2021-05-20 11:49:00 +0900

    style: clean up webaudio

--- src/features/editor/components/GuitarHeroView.tsx
+++ src/features/editor/components/GuitarHeroView.tsx
@@ -8,3 +8,15 @@ import {
 } from "../services/guitarHeroLanes";
 import styles from "./GuitarHeroView.module.css";
 
+const LOOK_AHEAD_SECONDS = 2.5;
+const LOOK_BEHIND_SECONDS = 0.35;
+const STRIKE_PERCENT = 85;
+const SUSTAIN_MIN_SECONDS = 0.25;
+
+interface GuitarHeroViewProps {
+  currentTime: number;
+  track: TabTrack;
+}
+
+export function GuitarHeroView({ currentTime, track }: GuitarHeroViewProps) {
+  const ghTrack = useMemo(() => eventsToGhTrack(track.events), [track.events]);
