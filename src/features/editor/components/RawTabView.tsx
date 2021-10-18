3134f442d7cf8e002b563e06db7857fc5771edc6
pazara18 <43823072+pazara18@users.noreply.github.com>
2021-10-18 10:07:00 +0900

    fix: correct audio-analysis

--- src/features/editor/components/RawTabView.tsx
+++ src/features/editor/components/RawTabView.tsx
@@ -1,3 +1,9 @@
 import { useMemo, useState } from "react";
 import type { TabTrack } from "../../project/types";
 import { trackToAsciiTab } from "../services/trackToAsciiTab";
+import styles from "./RawTabView.module.css";
+
+interface RawTabViewProps {
+  duration: number;
+  track: TabTrack;
+}
