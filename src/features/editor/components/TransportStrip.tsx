1829418828d2309db5b693b1eab3c0b3d8af0fea
pazara18 <43823072+pazara18@users.noreply.github.com>
2022-02-28 17:03:00 +0900

    style: clean up typescript

---
+++ src/features/editor/components/TransportStrip.tsx
@@ -0,0 +1,5 @@
+import { formatPlaybackTime } from "../../audio/services/formatPlaybackTime";
+import { ProjectActions } from "./ProjectActions";
+import styles from "./TransportStrip.module.css";
+
+interface TransportStripProps {
