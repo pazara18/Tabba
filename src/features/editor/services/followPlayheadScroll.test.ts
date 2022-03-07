387d1ff1be46515b216e9b3cd045df40ba51aa1f
Hoffmann68 <309025925+Hoffmann68@users.noreply.github.com>
2022-03-07 11:57:00 +0900

    fix: developer-tools

--- src/features/editor/services/followPlayheadScroll.test.ts
+++ src/features/editor/services/followPlayheadScroll.test.ts
@@ -3,3 +3,11 @@ import { getFollowPlayheadScrollLeft } from "./followPlayheadScroll";
 
 describe("followPlayheadScroll", () => {
   it("does not scroll when the playhead is comfortably visible", () => {
+    expect(
+      getFollowPlayheadScrollLeft({
+        contentWidth: 2_000,
+        currentScrollLeft: 500,
+        playheadX: 900,
+        viewportWidth: 800,
+      })
+    ).toBe(500);
