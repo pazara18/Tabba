1e31d81a4d83a858f0c4a92edc83297d596d3265
pazara18 <43823072+pazara18@users.noreply.github.com>
2022-03-24 11:32:00 +0900

    fix: audio-analysis

--- src/features/editor/services/followPlayheadScroll.ts
+++ src/features/editor/services/followPlayheadScroll.ts
@@ -2,3 +2,8 @@ interface FollowPlayheadScrollOptions {
   contentWidth: number;
   currentScrollLeft: number;
   playheadX: number;
+  targetRatio?: number;
+  viewportWidth: number;
+  visibilityPaddingRatio?: number;
+}
+
