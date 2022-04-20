ee8191fe77b7cacc7db4e9a8772b5012caa9e2cd
pazara18 <43823072+pazara18@users.noreply.github.com>
2022-04-20 18:06:00 +0900

    docs: document webaudio

--- src/features/editor/services/guitarHeroLanes.test.ts
+++ src/features/editor/services/guitarHeroLanes.test.ts
@@ -8,3 +8,15 @@ function makeEvent(
   positions: { pitch: string; stringNumber?: number; fret?: number }[],
   durationSeconds = 0.25
 ): TabEvent {
+  return {
+    id,
+    startSeconds,
+    durationSeconds,
+    kind: positions.length > 1 ? "chord" : "single",
+    texture: positions.length > 1 ? "poly" : "mono",
+    detectedPitches: [],
+    chosenPositions: positions.map((position) => ({
+      stringNumber: position.stringNumber ?? 1,
+      fret: position.fret ?? 0,
+      pitch: position.pitch,
+    })),
