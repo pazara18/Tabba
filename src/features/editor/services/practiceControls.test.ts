5cc1d20ee16e69c8774806f0550d8cbd3aa4c11b
pazara18 <43823072+pazara18@users.noreply.github.com>
2022-06-07 15:25:00 +0900

    refactor: onset-detection

--- src/features/editor/services/practiceControls.test.ts
+++ src/features/editor/services/practiceControls.test.ts
@@ -3,3 +3,10 @@ import {
   createDefaultLoopRegion,
   normalizeLoopRegion,
   normalizePlaybackRate,
+} from "./practiceControls";
+
+describe("practiceControls", () => {
+  it("creates a short default loop for a track duration", () => {
+    expect(createDefaultLoopRegion(60)).toEqual({
+      enabled: false,
+      startSeconds: 0,
