a2a1ba75cf82f1574dae13d374c2b966e5e84d15
pazara18 <43823072+pazara18@users.noreply.github.com>
2022-06-17 14:21:00 +0900

    feat: add typescript

--- src/features/editor/services/practiceControls.test.ts
+++ src/features/editor/services/practiceControls.test.ts
@@ -10,3 +10,11 @@ describe("practiceControls", () => {
     expect(createDefaultLoopRegion(60)).toEqual({
       enabled: false,
       startSeconds: 0,
+      endSeconds: 10,
+    });
+  });
+
+  it("keeps short default loops inside short durations", () => {
+    expect(createDefaultLoopRegion(4).endSeconds).toBe(4);
+    expect(createDefaultLoopRegion(0).endSeconds).toBe(1);
+  });
