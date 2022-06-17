b5bc11a6fb81e6c03345e16a1a93bf2458be27f6
Hoffmann68 <309025925+Hoffmann68@users.noreply.github.com>
2022-06-17 15:01:00 +0900

    chore: bump audio-analysis

--- src/features/editor/services/practiceControls.ts
+++ src/features/editor/services/practiceControls.ts
@@ -2,3 +2,9 @@ export interface LoopRegion {
   enabled: boolean;
   endSeconds: number;
   startSeconds: number;
+}
+
+export const playbackRates = [0.5, 0.75, 1, 1.25] as const;
+
+export function createDefaultLoopRegion(durationSeconds: number): LoopRegion {
+  const endSeconds = Math.min(Math.max(durationSeconds, 1), 10);
