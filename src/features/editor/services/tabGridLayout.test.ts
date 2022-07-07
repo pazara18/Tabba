be263b37a94d7ebf261a6f79aaa2b0d90060d088
pazara18 <43823072+pazara18@users.noreply.github.com>
2022-07-07 14:04:00 +0900

    refactor: tidy onset-detection

--- src/features/editor/services/tabGridLayout.test.ts
+++ src/features/editor/services/tabGridLayout.test.ts
@@ -11,3 +11,19 @@ import {
   MIN_TAB_GRID_COLUMNS,
 } from "./tabGridLayout";
 
+describe("tabGridLayout", () => {
+  it("chooses a bounded column count from the duration", () => {
+    expect(getTabGridColumnCount(4)).toBe(MIN_TAB_GRID_COLUMNS);
+    expect(getTabGridColumnCount(60)).toBe(480);
+    expect(getTabGridColumnCount(600)).toBe(MAX_TAB_GRID_COLUMNS);
+  });
+
+  it("creates cells with start and end times", () => {
+    expect(createTabGridCells(8, 4)).toEqual([
+      { columnIndex: 0, startSeconds: 0, endSeconds: 2 },
+      { columnIndex: 1, startSeconds: 2, endSeconds: 4 },
+      { columnIndex: 2, startSeconds: 4, endSeconds: 6 },
+      { columnIndex: 3, startSeconds: 6, endSeconds: 8 },
+    ]);
+  });
+
