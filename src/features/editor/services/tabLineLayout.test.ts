7d9a42394b33118c95c08f633cdbb706449901b7
pazara18 <43823072+pazara18@users.noreply.github.com>
2022-08-31 15:07:00 +0900

    fix: correct onset-detection

--- src/features/editor/services/tabLineLayout.test.ts
+++ src/features/editor/services/tabLineLayout.test.ts
@@ -16,3 +16,14 @@ function makeEvent(id: string, startSeconds: number): TabEvent {
     texture: "mono",
     detectedPitches: [],
     chosenPositions: [],
+    candidates: [],
+    confidence: 1,
+    locked: false,
+  };
+}
+
+describe("createTabLines", () => {
+  it("creates one line per chunk of the total duration", () => {
+    const lines = createTabLines(20, 8);
+
+    expect(lines).toHaveLength(3);
