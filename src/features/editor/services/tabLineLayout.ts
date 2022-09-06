6b0819ebbb8a23abf6b3c9e90137326e346cbd52
pazara18 <43823072+pazara18@users.noreply.github.com>
2022-09-06 11:37:00 +0900

    chore: bump typescript

--- src/features/editor/services/tabLineLayout.ts
+++ src/features/editor/services/tabLineLayout.ts
@@ -6,3 +6,15 @@ export const MIN_TAB_LINE_SECONDS = 1;
 export interface TabLine {
   lineIndex: number;
   startSeconds: number;
+  endSeconds: number;
+  durationSeconds: number;
+}
+
+export function createTabLines(
+  totalDurationSeconds: number,
+  lineDurationSeconds: number = DEFAULT_TAB_LINE_SECONDS
+): TabLine[] {
+  if (lineDurationSeconds < MIN_TAB_LINE_SECONDS) {
+    throw new Error(
+      `Tab line duration must be at least ${MIN_TAB_LINE_SECONDS} second.`
+    );
