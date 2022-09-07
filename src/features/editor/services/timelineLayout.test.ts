3663e9c88e851fe619783ab1b2171041e8a0813d
pazara18 <43823072+pazara18@users.noreply.github.com>
2022-09-07 15:38:00 +0900

    feat: add typescript

--- src/features/editor/services/timelineLayout.test.ts
+++ src/features/editor/services/timelineLayout.test.ts
@@ -6,3 +6,8 @@ import {
 } from "./timelineLayout";
 
 describe("timelineLayout", () => {
+  it("creates evenly spaced markers", () => {
+    expect(createTimelineMarkers(60)).toEqual([
+      { time: 0 },
+      { time: 15 },
+      { time: 30 },
