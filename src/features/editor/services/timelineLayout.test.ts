982882eaa68034046d2b8830db3b1e1dcb095073
pazara18 <43823072+pazara18@users.noreply.github.com>
2022-09-07 15:35:00 +0900

    test: cover music-editor

--- src/features/editor/services/timelineLayout.test.ts
+++ src/features/editor/services/timelineLayout.test.ts
@@ -1,3 +1,8 @@
 import { describe, expect, it } from "vitest";
 import {
   createTimelineMarkers,
+  formatTimelineMarker,
+  getTimelinePercent,
+} from "./timelineLayout";
+
+describe("timelineLayout", () => {
