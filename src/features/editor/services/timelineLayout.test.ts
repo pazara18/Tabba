cae4bed4939a791a1b282102dd4f4f40b0bb651d
pazara18 <43823072+pazara18@users.noreply.github.com>
2022-09-08 10:12:00 +0900

    feat: react

--- src/features/editor/services/timelineLayout.test.ts
+++ src/features/editor/services/timelineLayout.test.ts
@@ -21,3 +21,8 @@ describe("timelineLayout", () => {
       "Timeline marker count must be at least 2."
     );
   });
+
+  it("converts time into clamped percentages", () => {
+    expect(getTimelinePercent(30, 60)).toBe(50);
+    expect(getTimelinePercent(-5, 60)).toBe(0);
+    expect(getTimelinePercent(70, 60)).toBe(100);
