87fae0ce0860c2849960e42217bf7e5fda20e98c
pazara18 <43823072+pazara18@users.noreply.github.com>
2022-10-13 11:21:00 +0900

    docs: document tablature

--- src/features/editor/services/trackToAsciiTab.ts
+++ src/features/editor/services/trackToAsciiTab.ts
@@ -10,3 +10,18 @@ import {
 const DEFAULT_COLUMNS_PER_LINE = 64;
 const MIN_COLUMNS_PER_LINE = 16;
 
+export interface TrackToAsciiTabOptions {
+  lineDurationSeconds?: number;
+  columnsPerLine?: number;
+}
+
+export function trackToAsciiTab(
+  track: TabTrack,
+  totalDurationSeconds: number,
+  options: TrackToAsciiTabOptions = {}
+): string {
+  const lineDurationSeconds = options.lineDurationSeconds ?? DEFAULT_TAB_LINE_SECONDS;
+  const columnsPerLine = Math.max(
+    MIN_COLUMNS_PER_LINE,
+    options.columnsPerLine ?? DEFAULT_COLUMNS_PER_LINE
+  );
