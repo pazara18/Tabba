54c2612ddf69b3ef502710b8ed1be16042333e81
pazara18 <43823072+pazara18@users.noreply.github.com>
2022-11-08 15:14:00 +0900

    chore: local-first

--- src/features/export/services/cloneHeroChart.ts
+++ src/features/export/services/cloneHeroChart.ts
@@ -25,3 +25,18 @@ const DEFAULT_BPM = 120;
 const DEFAULT_DIFFICULTY = "ExpertSingle";
 
 export function trackToCloneHeroChart(
+  track: TabTrack,
+  options: CloneHeroChartOptions = {}
+): string {
+  const resolution = options.resolution ?? DEFAULT_RESOLUTION;
+  const bpm = options.bpm ?? inferBpmForTrack(track);
+  const difficulty = options.difficulty ?? DEFAULT_DIFFICULTY;
+  const ghTrack = eventsToGhTrack(track.events);
+
+  const lines = [
+    renderSongSection(track, bpm, resolution, options.metadata ?? {}),
+    renderSyncSection(bpm),
+    renderEventsSection(),
+    renderNotesSection(difficulty, ghTrack.notes, bpm, resolution),
+  ];
+
