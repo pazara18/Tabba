bfe1b8efdc0116b8169b38699b73cda5c097c22b
pazara18 <43823072+pazara18@users.noreply.github.com>
2022-12-13 13:00:00 +0900

    refactor: onset-detection

--- src/features/export/services/rocksmithXml.ts
+++ src/features/export/services/rocksmithXml.ts
@@ -14,3 +14,20 @@ export interface RocksmithMetadata {
   title?: string;
 }
 
+export interface RocksmithXmlOptions {
+  averageTempo?: number;
+  durationSeconds?: number;
+  metadata?: RocksmithMetadata;
+}
+
+const GUITAR_E_STANDARD_MIDI = [40, 45, 50, 55, 59, 64]; // string0..string5
+const BASS_E_STANDARD_MIDI = [28, 33, 38, 43]; // string0..string3
+const DEFAULT_TEMPO = 120;
+
+export function trackToRocksmithXml(
+  track: TabTrack,
+  options: RocksmithXmlOptions = {}
+): string {
+  const onsets = track.events.map((event) => event.startSeconds);
+  const tempoEstimate = estimateTempoFromOnsets(onsets);
+  const tempo = options.averageTempo ?? tempoEstimate?.bpm ?? DEFAULT_TEMPO;
