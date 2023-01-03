e8981186469a6f5d200921c23027f82abbf91815
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-01-03 17:44:00 +0900

    refactor: tidy local-first

--- src/features/project/services/createManualTabEvent.test.ts
+++ src/features/project/services/createManualTabEvent.test.ts
@@ -5,3 +5,14 @@ import { createManualTabEvent } from "./createManualTabEvent";
 describe("createManualTabEvent", () => {
   it("creates a locked single-note event from a string and fret", () => {
     const event = createManualTabEvent({
+      createId: () => "event-1",
+      fret: 2,
+      startSeconds: 12.5,
+      stringNumber: 4,
+      tuning: standardGuitarTuning,
+    });
+
+    expect(event).toMatchObject({
+      id: "event-1",
+      startSeconds: 12.5,
+      durationSeconds: 1,
