f61f34911cdb3a57960196387ee87a7bb49ae333
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-01-21 10:38:00 +0900

    perf: improve tablature

--- src/features/project/services/createManualTabEvent.ts
+++ src/features/project/services/createManualTabEvent.ts
@@ -4,3 +4,11 @@ import type { InstrumentTuning } from "../../../domain/instruments/types";
 import type { TabEvent } from "../../../domain/tab/types";
 
 interface CreateManualTabEventOptions {
+  createId?: () => string;
+  durationSeconds?: number;
+  fret: number;
+  startSeconds: number;
+  stringNumber: number;
+  tuning: InstrumentTuning;
+}
+
