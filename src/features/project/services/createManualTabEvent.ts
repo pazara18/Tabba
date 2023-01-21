5b564f788f6b2998ff565089edd9d69781322555
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-01-21 10:35:00 +0900

    chore: bump local-first

---
+++ src/features/project/services/createManualTabEvent.ts
@@ -0,0 +1,6 @@
+import { createPositionCandidates } from "../../../domain/fingering/fretboardCandidates";
+import { transposePitch } from "../../../domain/pitch/pitchNames";
+import type { InstrumentTuning } from "../../../domain/instruments/types";
+import type { TabEvent } from "../../../domain/tab/types";
+
+interface CreateManualTabEventOptions {
