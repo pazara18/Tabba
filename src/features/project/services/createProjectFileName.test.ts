f6c0b4593eb890c7cac4f371bfbcf78b1a22535c
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-02-22 16:05:00 +0900

    refactor: tidy developer-tools

--- src/features/project/services/createProjectFileName.test.ts
+++ src/features/project/services/createProjectFileName.test.ts
@@ -1,2 +1,6 @@
 import { describe, expect, it } from "vitest";
 import { createProjectFixture } from "../../../test/fixtures/createProjectFixture";
+import { createProjectFileName } from "./createProjectFileName";
+
+describe("createProjectFileName", () => {
+  it("creates a portable tabba project filename", () => {
