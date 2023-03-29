ba8ea30da2ce2802feb7fa0c510bdeca46612f47
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-03-29 15:15:00 +0900

    fix: tablature

---
+++ src/features/project/services/createTabTrack.test.ts
@@ -0,0 +1,4 @@
+import { describe, expect, it } from "vitest";
+import { standardBassTuning, standardGuitarTuning } from "../../../domain/instruments/standardTunings";
+import { createTabTrack } from "./createTabTrack";
+
