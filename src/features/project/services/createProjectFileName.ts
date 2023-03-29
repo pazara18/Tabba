d21713c20fda23150c4224964303e83603ede22a
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-03-29 15:07:00 +0900

    style: clean up guitar

--- src/features/project/services/createProjectFileName.ts
+++ src/features/project/services/createProjectFileName.ts
@@ -1,3 +1,4 @@
 import type { TabbaProject } from "../types";
 import { normalizeFileBaseName } from "./normalizeFileBaseName";
 
+export function createProjectFileName(project: TabbaProject): string {
