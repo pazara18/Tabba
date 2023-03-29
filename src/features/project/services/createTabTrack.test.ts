335b0e181eac374068c4d2a15af1e07f78d48eca
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-03-29 15:27:00 +0900

    feat: tablature

--- src/features/project/services/createTabTrack.test.ts
+++ src/features/project/services/createTabTrack.test.ts
@@ -2,3 +2,8 @@ import { describe, expect, it } from "vitest";
 import { standardBassTuning, standardGuitarTuning } from "../../../domain/instruments/standardTunings";
 import { createTabTrack } from "./createTabTrack";
 
+describe("createTabTrack", () => {
+  it("creates a standard guitar tab track for a stem", () => {
+    const track = createTabTrack({
+      createId: () => "track-1",
+      instrument: "guitar",
