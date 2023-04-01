a5ec08e3ad10f85d4db5ae73e3ece95fe52c484a
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-04-01 10:00:00 +0900

    chore: bump music-editor

--- src/features/project/services/createTabTrack.ts
+++ src/features/project/services/createTabTrack.ts
@@ -1,2 +1,6 @@
 import type { InstrumentKind } from "../../../domain/instruments/types";
 import { standardBassTuning, standardGuitarTuning } from "../../../domain/instruments/standardTunings";
+import type { TabTrack } from "../types";
+
+interface CreateTabTrackOptions {
+  createId?: () => string;
