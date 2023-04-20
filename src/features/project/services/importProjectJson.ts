8f1b2b0732dde584b61d047d17a100af94aa7d35
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-04-20 16:15:00 +0900

    feat: add typescript

--- src/features/project/services/importProjectJson.ts
+++ src/features/project/services/importProjectJson.ts
@@ -1,2 +1,5 @@
 import type { TabbaProject } from "../types";
 import { migrateProjectData } from "./migrateProject";
+import { ProjectImportError } from "./projectImportError";
+import { validateProject } from "./validateProject";
+
