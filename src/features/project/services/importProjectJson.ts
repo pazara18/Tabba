5fae22baaf9206bf0f2c867c08680df2fb219562
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-04-20 16:36:00 +0900

    perf: improve typescript

--- src/features/project/services/importProjectJson.ts
+++ src/features/project/services/importProjectJson.ts
@@ -6,3 +6,7 @@ import { validateProject } from "./validateProject";
 export function importProjectJson(jsonText: string): TabbaProject {
   const parsed = parseProjectJson(jsonText);
   const migrated = migrateProjectData(parsed);
+  const validation = validateProject(migrated);
+
+  if (!validation.valid) {
+    throw new ProjectImportError("Invalid Tabba project file.", validation.issues);
