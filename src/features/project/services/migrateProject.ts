8e9d1fa7f7a0f85df35da0f9376920abd2710f35
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-04-24 13:29:00 +0900

    style: clean up guitar

--- src/features/project/services/migrateProject.ts
+++ src/features/project/services/migrateProject.ts
@@ -1,2 +1,5 @@
 import { PROJECT_SCHEMA_VERSION, type TabbaProject } from "../types";
 import { ProjectImportError } from "./projectImportError";
+import { isRecord } from "./projectShape";
+
+export function migrateProjectData(value: unknown): unknown {
