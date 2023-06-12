babd4af0ea6ad87d0cbe07dfe987cddbce474d00
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-06-12 14:42:00 +0900

    refactor: tidy guitar

---
+++ src/features/project/services/updateProjectStems.test.ts
@@ -0,0 +1,3 @@
+import { describe, expect, it } from "vitest";
+import { createProjectFixture } from "../../../test/fixtures/createProjectFixture";
+import { addStemToProject, setStemDuration } from "./updateProjectStems";
