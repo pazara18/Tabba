6f3256b6a136e906cc8355ce130169002bcbfd8b
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-06-15 09:24:00 +0900

    feat: add webaudio

--- src/features/project/services/updateProjectStems.test.ts
+++ src/features/project/services/updateProjectStems.test.ts
@@ -5,3 +5,7 @@ import { addStemToProject, setStemDuration } from "./updateProjectStems";
 describe("updateProjectStems", () => {
   it("adds a stem without mutating the original project", () => {
     const project = createProjectFixture();
+    const updated = addStemToProject(
+      project,
+      { id: "stem-2", name: "Bass", offsetSeconds: 0 },
+      new Date("2026-04-15T15:00:00.000Z")
