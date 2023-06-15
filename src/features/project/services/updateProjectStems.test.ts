c543126c74e3e37e8434d16f2e04166ec4dd0fa9
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-06-15 09:21:00 +0900

    chore: music-editor

--- src/features/project/services/updateProjectStems.test.ts
+++ src/features/project/services/updateProjectStems.test.ts
@@ -1,3 +1,7 @@
 import { describe, expect, it } from "vitest";
 import { createProjectFixture } from "../../../test/fixtures/createProjectFixture";
 import { addStemToProject, setStemDuration } from "./updateProjectStems";
+
+describe("updateProjectStems", () => {
+  it("adds a stem without mutating the original project", () => {
+    const project = createProjectFixture();
