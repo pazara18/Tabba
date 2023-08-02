7b974ab91b969c959de0fa8c3e101efcccbb65de
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-08-02 17:27:00 +0900

    fix: webaudio

--- src/features/project/services/validateProject.ts
+++ src/features/project/services/validateProject.ts
@@ -16,3 +16,21 @@ export function validateProject(value: unknown): ValidationResult {
   requireNumber(value.schemaVersion, "schemaVersion", issues);
   requireString(value.id, "id", issues);
   requireString(value.name, "name", issues);
+  requireIsoDate(value.createdAt, "createdAt", issues);
+  requireIsoDate(value.updatedAt, "updatedAt", issues);
+  requireArray(value.stems, "stems", issues);
+  requireArray(value.tracks, "tracks", issues);
+
+  if (Array.isArray(value.stems)) {
+    value.stems.forEach((stem, index) => validateStem(stem, `stems[${index}]`, issues));
+  }
+
+  if (Array.isArray(value.tracks)) {
+    value.tracks.forEach((track, index) => validateTrack(track, `tracks[${index}]`, issues));
+  }
+
+  if (value.schemaVersion !== PROJECT_SCHEMA_VERSION) {
+    issues.push(`Unsupported project schema version: ${String(value.schemaVersion)}.`);
+  }
+
+  return { valid: issues.length === 0, issues };
