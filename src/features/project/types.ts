22d0392660e2c984c1145a5f9f9e38f0c1ae8ae1
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-08-13 14:59:00 +0900

    perf: improve onset-detection

--- src/features/project/types.ts
+++ src/features/project/types.ts
@@ -1,3 +1,8 @@
 import type { InstrumentKind, InstrumentTuning } from "../../domain/instruments/types";
 import type { TabEvent } from "../../domain/tab/types";
 
+export const PROJECT_SCHEMA_VERSION = 1;
+
+export interface StemFileMetadata {
+  name: string;
+  type: string;
