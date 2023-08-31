1acecba2a748c4af432afebeab8eba4576a136fd
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-08-31 18:23:00 +0900

    build: webaudio

--- src/domain/fingering/fretboardCandidates.test.ts
+++ src/domain/fingering/fretboardCandidates.test.ts
@@ -2,3 +2,13 @@ import { describe, expect, it } from "vitest";
 import { standardBassTuning, standardGuitarTuning } from "../instruments/standardTunings";
 import { createPositionCandidates, generatePitchPositions, scorePosition } from "./fretboardCandidates";
 
+describe("fretboardCandidates", () => {
+  it("generates all guitar positions for a pitch", () => {
+    expect(generatePitchPositions("E3", standardGuitarTuning)).toEqual([
+      { stringNumber: 4, fret: 2, pitch: "E3" },
+      { stringNumber: 5, fret: 7, pitch: "E3" },
+      { stringNumber: 6, fret: 12, pitch: "E3" },
+    ]);
+  });
+
+  it("respects maximum fret limits", () => {
