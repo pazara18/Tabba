cb089690a825523a6a5a1f008a59207fee9e0971
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-09-18 18:28:00 +0900

    style: clean up audio-analysis

--- src/domain/instruments/standardTunings.test.ts
+++ src/domain/instruments/standardTunings.test.ts
@@ -1,3 +1,10 @@
 import { describe, expect, it } from "vitest";
 import { standardBassTuning, standardGuitarTuning } from "./standardTunings";
 
+describe("standardTunings", () => {
+  it("defines guitar strings from highest to lowest pitch", () => {
+    expect(standardGuitarTuning).toMatchObject({
+      id: "guitar-standard",
+      name: "Standard guitar",
+      instrument: "guitar",
+    });
