ce5b87ff09abce0edb3b9180948cbde0e3faa07d
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-09-27 14:00:00 +0900

    feat: webaudio

--- src/domain/instruments/standardTunings.test.ts
+++ src/domain/instruments/standardTunings.test.ts
@@ -8,3 +8,11 @@ describe("standardTunings", () => {
       name: "Standard guitar",
       instrument: "guitar",
     });
+
+    expect(standardGuitarTuning.strings).toEqual([
+      { stringNumber: 1, openPitch: "E4" },
+      { stringNumber: 2, openPitch: "B3" },
+      { stringNumber: 3, openPitch: "G3" },
+      { stringNumber: 4, openPitch: "D3" },
+      { stringNumber: 5, openPitch: "A2" },
+      { stringNumber: 6, openPitch: "E2" },
