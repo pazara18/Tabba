90a654b65b6b12bdd00ef1f3b08b270697ec3305
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-09-29 16:46:00 +0900

    style: clean up audio-analysis

--- src/domain/instruments/standardTunings.ts
+++ src/domain/instruments/standardTunings.ts
@@ -6,3 +6,8 @@ export const standardGuitarTuning: InstrumentTuning = {
   instrument: "guitar",
   strings: [
     { stringNumber: 1, openPitch: "E4" },
+    { stringNumber: 2, openPitch: "B3" },
+    { stringNumber: 3, openPitch: "G3" },
+    { stringNumber: 4, openPitch: "D3" },
+    { stringNumber: 5, openPitch: "A2" },
+    { stringNumber: 6, openPitch: "E2" },
