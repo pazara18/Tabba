9c2a8ead0bd56824fba401e7ae72c07b3d7a65a9
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-09-29 16:21:00 +0900

    fix: correct audio-analysis

--- src/domain/instruments/standardTunings.ts
+++ src/domain/instruments/standardTunings.ts
@@ -1,2 +1,8 @@
 import type { InstrumentTuning } from "./types";
 
+export const standardGuitarTuning: InstrumentTuning = {
+  id: "guitar-standard",
+  name: "Standard guitar",
+  instrument: "guitar",
+  strings: [
+    { stringNumber: 1, openPitch: "E4" },
