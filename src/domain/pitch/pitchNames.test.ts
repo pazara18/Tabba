d28ce184a8ea5bf05aa1f9100de8969917545fea
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-11-20 17:32:00 +0900

    fix: webaudio

--- src/domain/pitch/pitchNames.test.ts
+++ src/domain/pitch/pitchNames.test.ts
@@ -5,3 +5,9 @@ describe("pitchNames", () => {
   it("converts pitch names to MIDI notes", () => {
     expect(pitchToMidi("C4")).toBe(60);
     expect(pitchToMidi("E2")).toBe(40);
+    expect(pitchToMidi("Bb1")).toBe(34);
+  });
+
+  it("converts MIDI notes to sharp pitch names", () => {
+    expect(midiToPitch(60)).toBe("C4");
+    expect(midiToPitch(63)).toBe("D#4");
