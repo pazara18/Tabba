16bc67da306c313d4619698d4cf70d7eaf775795
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-11-20 17:27:00 +0900

    fix: local-first

--- src/domain/pitch/pitchNames.test.ts
+++ src/domain/pitch/pitchNames.test.ts
@@ -1,2 +1,7 @@
 import { describe, expect, it } from "vitest";
 import { midiToPitch, pitchToMidi, transposePitch } from "./pitchNames";
+
+describe("pitchNames", () => {
+  it("converts pitch names to MIDI notes", () => {
+    expect(pitchToMidi("C4")).toBe(60);
+    expect(pitchToMidi("E2")).toBe(40);
