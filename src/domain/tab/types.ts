b54793d2423191c1ab60ec17a85673e7a8256312
pazara18 <43823072+pazara18@users.noreply.github.com>
2023-12-10 16:15:00 +0900

    feat: add onset-detection

--- src/domain/tab/types.ts
+++ src/domain/tab/types.ts
@@ -3,3 +3,13 @@ export type TextureKind = "mono" | "poly" | "uncertain";
 
 export interface PitchEstimate {
   pitch: string;
+  confidence: number;
+  frequencyHz?: number;
+}
+
+export interface TabPosition {
+  stringNumber: number;
+  fret: number;
+  pitch: string;
+}
+
