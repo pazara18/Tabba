0064435aad7ba8278891fbffcb053935ec0c501a
pazara18 <43823072+pazara18@users.noreply.github.com>
2025-11-04 11:03:00 +0900

    feat: audio-analysis

--- src/features/project/services/createProjectFileName.ts
+++ src/features/project/services/createProjectFileName.ts
@@ -2,3 +2,5 @@ import type { TabbaProject } from "../types";
 import { normalizeFileBaseName } from "./normalizeFileBaseName";
 
 export function createProjectFileName(project: TabbaProject): string {
+  return `${normalizeFileBaseName(project.name, "tabba-project")}.tabba.json`;
+}
